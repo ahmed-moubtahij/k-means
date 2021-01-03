38de0e9e3a0886447ffc317dc6fe09f5e88c1aa9
Ayenem <50707385+Ayenem@users.noreply.github.com>
2021-01-02 22:31:48 -0500

    Update K_means.h

--- K_means.h
+++ K_means.h
@@ -247,5 +247,5 @@ int main(){
 
     return 0;
 }
-//My thanks to the #includecpp Discord's folk:
+//My thanks to the #includecpp Discord's folk for teaching me so much about C++:
 //Sarah, Léo, marcorubini and oktal
