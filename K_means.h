a955ea59e954cf9ab124c2f1978497b49dc8f168
Ayenem <50707385+Ayenem@users.noreply.github.com>
2021-01-06 21:46:10 -0500

    Update K_means.h

--- K_means.h
+++ K_means.h
@@ -9,7 +9,23 @@
 
 #define FWD(x) static_cast<decltype(x)&&>(x)
 
+//TODO: the std::array overload should be a std::span (also readup on diff with contiguous_range)
+
+//sarah notes:
+// instead of using the range member functions, you should be using the std::ranges::* function objects
+// for instance rn::begin(*) instead of *.begin()
+
+// i also wouldn't bother with using cbegin and cend
+// they provide a false sense of security
+// since views have shallow const semantics
+
+//TODO: marcorubini's function object wrapper implementation
+//TODO: Lesley Lai's non-generic lambdas
+
 using fmt::print;
+
+template<typename ...Ts>
+[[deprecated]] constexpr bool print_type = true;
 
 //dispatch: Akin to a std::partition_copy albeit with output to multiple ranges
 //src_range should be const& but ranges::views don't support const begin/end
@@ -54,6 +70,7 @@ struct DataPoint final : private std::array<T, D> {
     using std::array<T, D>::cbegin;
     using std::array<T, D>::cend;
     using std::array<T, D>::operator[];
+    using std::array<T, D>::size;
 
     constexpr DataPoint() noexcept = default;
     constexpr DataPoint(DataPoint const&) noexcept = default;
@@ -111,8 +128,8 @@ auto sqr_distance(DataPoint<T1,D> const& dp1,
     return
     std::transform_reduce(dp1.cbegin(), dp1.cend(),
                           dp2.cbegin(), 0,
-                          [](auto a, auto b){ return a + b; },
-                          [](auto a, auto b){ return (a - b) * (a - b); }
+                          [](auto a, auto b){ return a+b; },
+                          [](auto a, auto b){ return (a-b)*(a-b); }
                          );
 };
 //distance_from: Function Object Comparator
@@ -124,7 +141,7 @@ struct distance_from{
     
     distance_from() = delete;
     distance_from& operator=(distance_from const&) = delete;
-    constexpr distance_from(ref_point_t const& pt) : m_pt{ pt } {}
+    constexpr distance_from(ref_point_t const& pt) : m_pt{pt} {}
     
     template<typename U>
     constexpr bool operator()(DataPoint<U, D> const& c1,
@@ -139,17 +156,18 @@ struct distance_from{
 };
 
 template<typename T, std::size_t D, std::size_t K>
-auto init_centroids(auto&& clusters, auto const& data)
+auto init_centroids(auto&& clusters, auto const& data_points)
 {
     using cluster_t = Cluster<T, D>;    
     auto centroids = FWD(clusters) | rnv::transform(&cluster_t::centroid);
-    //Initialize centroids with a sample of K points from data
+    //Initialize centroids with a sample of K points from data_points
     if constexpr(std::floating_point<T>){
-        rn::sample(data, centroids.begin(),
+        rn::sample(data_points, centroids.begin(),
                    K, std::mt19937{std::random_device{}()});
-    } else { //data is a range of DataPoints of integral value types T
-        //centroids get updated with data points' means so they are floating point types
-        rn::sample(data |
+    } else { //else, data_points is a range of DataPoints of integral value types T
+        //T needs to be a floating point type to match centroids' T,
+        //that is because centroids get updated with data_points' means
+        rn::sample(data_points |
                    rnv::transform([](auto const& pt)
                    { return static_cast<DataPoint<double, D>>(pt); }),
                    centroids.begin(),
@@ -165,7 +183,7 @@ void update_satellites(auto&& clusters,
     using cluster_t = Cluster<T, D>;
     
     rn::for_each(clusters,
-                 [](auto&& satellites){ FWD(satellites).clear(); },
+                 [](auto&& satellites){ FWD(satellites).clear();},
                  &cluster_t::satellites);
     
     auto constexpr find_closest_centroid =
@@ -173,7 +191,7 @@ void update_satellites(auto&& clusters,
     { return rn::min_element(FWD(clusters), comp, proj); };
 
     auto constexpr comp_dist_to_centroid =
-    [](auto const& data_pt){ return distance_from{ data_pt }; };
+    [](auto const& data_pt){ return distance_from{data_pt}; };
 
     //Dispatch every data point to the cluster whose centroid is closest
     dispatch(FWD(non_centroid_data),
@@ -201,43 +219,130 @@ void update_centroids(auto&& clusters)
                   mean, &cluster_t::satellites);
 }
 
-template<std::size_t K, std::size_t SZ,
-         arithmetic T, std::size_t D>
-constexpr auto
-k_means(std::array<DataPoint<T, D>, SZ> const& data, std::size_t n)
--> std::array<Cluster<T, D>, K>
+//k_means_impl: ARRAY overload
+template<std::size_t K, arithmetic PT_VALUE_T,
+         std::size_t D, std::size_t SZ>
+constexpr auto k_means_ovrl(std::array<DataPoint<PT_VALUE_T, D>, SZ> const& data_points, std::size_t n)
+-> std::array<Cluster<PT_VALUE_T, D>, K>
 {
+    fmt::print("CALLING STD::ARRAY OVERLOAD\n\n");
     if (SZ < K or K < 2) return {{}};
 
-    using cluster_t = Cluster<T, D>;
+    using cluster_t = Cluster<PT_VALUE_T, D>;
     std::array<cluster_t, K> clusters;
-    //A given cluster will have at most SZ satellites
-    rn::for_each(clusters,
-                 [](auto&& satellites){FWD(satellites).reserve(SZ); },
-                 &cluster_t::satellites);
 
-    auto centroids = init_centroids<T, D, K>(clusters, data);
+    auto centroids = init_centroids<PT_VALUE_T, D, K>(clusters, data_points);
         
     auto const is_not_centroid = [&centroids](auto const& pt)
     { return rn::find(centroids, static_cast<DataPoint<double, D>>(pt)) == centroids.end(); };
    
-    update_satellites<T, D>(clusters, data | rnv::filter(is_not_centroid));
+    update_satellites<PT_VALUE_T, D>(clusters, data_points | rnv::filter(is_not_centroid));
 
     while(n--){
-        update_centroids<T, D>(clusters);
-        update_satellites<T, D>(clusters, data | rnv::filter(is_not_centroid));
+        update_centroids<PT_VALUE_T, D>(clusters);
+        update_satellites<PT_VALUE_T, D>(clusters, data_points | rnv::filter(is_not_centroid));
     }
     
     return clusters;
 }
 
+//k_means_impl: RANGE overload
+template<std::size_t K, arithmetic PT_VALUE_T, std::size_t D>
+constexpr auto k_means_ovrl(auto const& data_points, std::size_t n)
+//TODO: Trailing return type
+{
+    fmt::print("CALLING RANGES OVERLOAD\n\n");
+    // if (std::ranges::distance(data_points) < K or K < 2) return {{}};
+    
+    //TODO: This shouldn't be a std::array but the data_points' decltype
+    std::array<Cluster<PT_VALUE_T, D>, K> clusters;
+
+    auto centroids = init_centroids<PT_VALUE_T, D, K>(clusters, data_points);
+        
+    auto const is_not_centroid = [&centroids/*, &D*/](auto const& pt)
+    { return rn::find(centroids, static_cast<DataPoint<double, D>>(pt)) == centroids.end(); };
+   
+    update_satellites<PT_VALUE_T, D>(clusters, data_points | rnv::filter(is_not_centroid));
+
+    while(n--){
+        update_centroids<PT_VALUE_T, D>(clusters);
+        update_satellites<PT_VALUE_T, D>(clusters, data_points | rnv::filter(is_not_centroid));
+    }
+    
+    return clusters;
+}
+
+template<typename T>
+struct is_data_point : std::false_type {};
+template<typename T, std::size_t D>
+struct is_data_point<DataPoint<T, D>> : std::true_type {};
+template<typename T>
+concept data_points_range = is_data_point<std::ranges::range_value_t<T>>::value;
+
+template<typename T>
+struct data_point_size;
+template<typename T, std::size_t D>
+struct data_point_size<DataPoint<T, D>> : std::integral_constant<std::size_t, D> {};
+
+template<std::size_t K>
+constexpr auto k_means(data_points_range auto const& data_points, std::size_t n)
+{
+    using data_point_t = rn::range_value_t<decltype(data_points)>;
+    using point_value_t = data_point_t::value_type;
+    auto constexpr dimension = data_point_size<data_point_t>::value;
+
+    return k_means_ovrl<K, point_value_t, dimension>(FWD(data_points), n);
+}
+
 } // namespace kmn
 
 int main(){
-    using std::array, kmn::DataPoint;
+    using std::array, std::vector, kmn::DataPoint;
     using kmn::print_clusters, kmn::k_means;
 
-    auto const integral_df = array{DataPoint(1, 2, 3),
+    auto const int_arr_df = array{DataPoint(1, 2, 3),
+                                  DataPoint(4, 5, 6),
+                                  DataPoint(7, 8, 9),
+                                  DataPoint(10, 11, 12),
+                                  DataPoint(13, 14, 15),
+                                  DataPoint(16, 17, 18),
+                                  DataPoint(19, 20, 21),
+                                  DataPoint(22, 23, 24),
+                                  DataPoint(25, 26, 27),
+                                  DataPoint(28, 29, 30),
+                                  DataPoint(31, 32, 33),
+                                  DataPoint(34, 35, 36),
+                                  DataPoint(37, 38, 39),
+                                  DataPoint(40, 41, 42)};
+    auto const double_arr_df = array{DataPoint(1.0, 2.0, 3.0),
+                                     DataPoint(4.0, 5.0, 6.0),
+                                     DataPoint(7.0, 8.0, 9.0),
+                                     DataPoint(10.0, 11.0, 12.0),
+                                     DataPoint(13.0, 14.0, 15.0),
+                                     DataPoint(16.0, 17.0, 18.0),
+                                     DataPoint(19.0, 20.0, 21.0),
+                                     DataPoint(22.0, 23.0, 24.0),
+                                     DataPoint(25.0, 26.0, 27.0),
+                                     DataPoint(28.0, 29.0, 30.0),
+                                     DataPoint(31.0, 32.0, 33.0),
+                                     DataPoint(34.0, 35.0, 36.0),
+                                     DataPoint(37.0, 38.0, 39.0),
+                                     DataPoint(40.0, 41.0, 42.0)};
+    auto const float_arr_df = array{DataPoint(1.0f, 2.0f, 3.0f),//TODO: Doesn't work with floats
+                                    DataPoint(4.0f, 5.0f, 6.0f),
+                                    DataPoint(7.0f, 8.0f, 9.0f),
+                                    DataPoint(10.0f, 11.0f, 12.0f),
+                                    DataPoint(13.0f, 14.0f, 15.0f),
+                                    DataPoint(16.0f, 17.0f, 18.0f),
+                                    DataPoint(19.0f, 20.0f, 21.0f),
+                                    DataPoint(22.0f, 23.0f, 24.0f),
+                                    DataPoint(25.0f, 26.0f, 27.0f),
+                                    DataPoint(28.0f, 29.0f, 30.0f),
+                                    DataPoint(31.0f, 32.0f, 33.0f),
+                                    DataPoint(34.0f, 35.0f, 36.0f),
+                                    DataPoint(37.0f, 38.0f, 39.0f),
+                                    DataPoint(40.0f, 41.0f, 42.0f)};
+    auto const int_vec_df = vector{DataPoint(1, 2, 3),
                                    DataPoint(4, 5, 6),
                                    DataPoint(7, 8, 9),
                                    DataPoint(10, 11, 12),
@@ -250,9 +355,9 @@ int main(){
                                    DataPoint(31, 32, 33),
                                    DataPoint(34, 35, 36),
                                    DataPoint(37, 38, 39),
-                                   DataPoint(40, 41, 42)};
+                                   DataPoint(40, 41, 42)};                                    
     print("OUTPUT clusters:\n\n");
-    print_clusters(k_means<4>(integral_df, 100));
+    print_clusters(k_means<4>(double_arr_df, 10));
 
     return 0;
 }
