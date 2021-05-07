5af43154a1da4bece30cdf17eb534c291f9e06e9
Ayenem <moub.ahmed@hotmail.com>
2021-05-07 18:56:00 -0400

    brought specific warnings back

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -57,10 +57,10 @@ function(set_project_warnings project_name)
   set(GCC_WARNINGS
       ${CLANG_WARNINGS}
       -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
-      # -Wduplicated-cond # warn if if / else chain has duplicated conditions
-      # -Wduplicated-branches # warn if if / else branches have duplicated code
-      # -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-      # -Wuseless-cast # warn if you perform a cast to the same type
+      -Wduplicated-cond # warn if if / else chain has duplicated conditions
+      -Wduplicated-branches # warn if if / else branches have duplicated code
+      -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+      -Wuseless-cast # warn if you perform a cast to the same type
   )
 
   if(MSVC)
