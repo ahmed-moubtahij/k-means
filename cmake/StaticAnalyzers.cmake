e0d37389f6496acf0a5678bf2e7c20b933587354
ayenem <moub.ahmed@hotmail.com>
2021-09-03 13:37:48 -0400

    Successful integration of Cppcheck to build

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,18 +1,19 @@
-option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
-option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Builds with clang-tidy, if available. Defaults to ON." ON)
+option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" ON)
+option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Builds with clang-tidy, if available. Defaults to ON." OFF)
 option(ENABLE_INCLUDE_WHAT_YOU_USE "Enable static analysis with include-what-you-use" OFF)
 
 if(ENABLE_CPPCHECK)
-  find_program(CPPCHECK cppcheck)
-  if(CPPCHECK)
+  find_program(CPPCHECK_PATH cppcheck /usr/local/share/Cppcheck)
+  if(CPPCHECK_PATH)
     set(CMAKE_CXX_CPPCHECK
-        ${CPPCHECK}
-        --suppress=missingInclude
+        ${CPPCHECK_PATH}
         --enable=all
+        --suppress=missingInclude
         --inline-suppr
         --inconclusive
         -i
         ${CMAKE_SOURCE_DIR}/imgui/lib)
+    message(STATUS "Cppcheck from ${CPPCHECK_PATH} is set")
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
